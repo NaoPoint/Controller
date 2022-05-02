cb276c6ff2c7304c76fa1e1b80372839f9d41a56
PellegattaSimone <pellegattasimone@outlook.com>
2022-05-02 18:51:50 +0200

    First commit

---
+++ stop.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+REM set dynamic IP address
+netsh interface ip set address name="Wi-Fi" dhcp
+
+REM stop flask server
+taskkill /f /im "python.exe" /fi "WINDOWTITLE eq Flask Server" >nul 2>nul
+
+REM run xampp
+C:\Xampp\apache\bin\httpd.exe -k stop >nul 2>nul
