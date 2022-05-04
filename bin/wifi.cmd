3c690845a665d11abf0bc7b507f2adb746b09218
PellegattaSimone <pellegattasimone@outlook.com>
2022-05-04 23:43:11 +0200

    Added confirmation message for wifi.cmd

--- bin/wifi.cmd
+++ bin/wifi.cmd
@@ -1,4 +1,9 @@
 @echo off
+title Wireless adapters set back to dhcp...
 
 REM set dynamic IP address
 netsh interface ip set address "Wi-Fi" dhcp >nul 2>nul
+
+REM confirmation message
+echo Success. & echo.
+pause
